42b98f70f8a867223333b24bfdc151810135cfa8
Arlene George <aemg@live.com>
2019-08-17 01:10:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,7 +15,7 @@ def map_to_square(source_array)
 end
     
 
-def reduce_to_total(source_array, starting_point = 0)
+def reduce_to_total(source_array, starting_point)
   source_array.reduce(0) { |sum, num| sum + num}
   source_array.reduce(100) { |sum, num| sum + num}
 end
