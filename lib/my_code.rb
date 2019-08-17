6328438fcec94299b81786121b9664549b900170
Arlene George <aemg@live.com>
2019-08-17 01:21:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,6 +16,6 @@ end
     
 source_array = [1,2,3]
 starting_point = 100
-def reduce_to_total(source_array, starting_point = 0)
+def reduce_to_total
   source_array.reduce(0) { |sum, num| sum + num}
 end
